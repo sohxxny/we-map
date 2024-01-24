c1445c8d41f7fd07f94766cf5c835450cb176006
sohxxny <sohxxny@gmail.com>
2024-01-24 16:57:12 +0900

    initial commit

---
+++ WeMap/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WeMap
+//
+//  Created by Lee Soheun on 2024/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
