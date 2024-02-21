b3a213f1c91980c5be4e1e7b10e241bcf993605d
sohxxny <sohxxny@gmail.com>
2024-02-21 12:19:07 +0900

    feat: add tab bar

---
+++ WeMap/MainTabBarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MainTabBarViewController.swift
+//  WeMap
+//
+//  Created by Lee Soheun on 2/21/24.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
