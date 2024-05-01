04af353f50b63c1068afd03ebb43ca573589ba3b
sohxxny <sohxxny@gmail.com>
2024-05-01 21:35:11 +0900

    notification 화면 추가

---
+++ Views/FriendsRequestTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  FriendsRequestTableViewCell.swift
+//  WeMap
+//
+//  Created by Lee Soheun on 5/1/24.
+//
+
+import UIKit
+
+class FriendsRequestTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var friendsRequestUserImage: UIImageView!
+    @IBOutlet weak var friendsRequestUserName: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        // 이미지 뷰를 원형으로 만들기
+        friendsRequestUserImage.layer.cornerRadius = friendsRequestUserImage.frame.width / 2
+        friendsRequestUserImage.clipsToBounds = true
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+    }
+
+}
