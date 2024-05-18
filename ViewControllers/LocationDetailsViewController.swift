e3b556bc08934a088021b6a034579b8533aea401
sohxxny <sohxxny@gmail.com>
2024-05-18 21:47:56 +0900

    컬렉션 뷰 셀 추가

--- ViewControllers/LocationDetailsViewController.swift
+++ ViewControllers/LocationDetailsViewController.swift
@@ -7,13 +7,34 @@
 
 import UIKit
 
-class LocationDetailsViewController: BaseViewController {
+class LocationDetailsViewController: BaseViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
     @IBOutlet weak var addressLabel: UILabel!
+    @IBOutlet weak var locationDetailsCollectionView: UICollectionView!
+    
+    let dataArray = ["서울과학기술대학교", "은광여고", "아주아주아주아주아주아주긴이름"]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        locationDetailsCollectionView.delegate = self
+        locationDetailsCollectionView.dataSource = self
     }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return dataArray.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let albumCell = locationDetailsCollectionView.dequeueReusableCell(withReuseIdentifier: "LocationDetailsAlbumCell", for: indexPath) as! LocationDetailsAlbumCell
+        
+        albumCell.featuredImage.image = UIImage(named: "bookmark-icon")
+        albumCell.albumName.text = dataArray[indexPath.row]
+        
+        return albumCell
+    }
+    
     
     func configure(with addressString: String) {
         addressLabel.text = addressString
